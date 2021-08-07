edc5b5bcc8b590ef288a5d5e7b2b569d7190a23e
Mmx <1624045573@qq.com>
2021-08-07 09:45:47 +0800

    style:移动初始化ws连接函数位置

---
+++ global/connect.go
@@ -0,0 +1,28 @@
+package global
+
+import (
+	"github.com/Mmx233/VpsBrokerC/models"
+	"github.com/gorilla/websocket"
+	"log"
+)
+
+func connect() error {
+	var e error
+	if Conn, _, e = websocket.DefaultDialer.Dial(
+		func() string {
+			return Config.RemoteAddr
+		}(), nil); e != nil {
+		log.Println(e)
+	}
+	return e
+}
+
+func init() {
+	for connect() != nil {
+	}
+	for Conn.WriteJSON(&models.VpsInit{
+		Name: Config.Name,
+		Port: Config.Port,
+	}) != nil {
+	}
+}
