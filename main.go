f0c0a807131407c6e1c36cfb8e51670a5a655e0a
Mmx <1624045573@qq.com>
2021-08-18 00:08:57 +0800

    fix:修正main.go端口监听缺失

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"github.com/Mmx233/VpsBrokerC/global"
 	"github.com/Mmx233/VpsBrokerC/router"
 	"log"
 )
 
 func main() {
-	if e := router.G.Run(); e != nil {
+	if e := router.G.Run(":" + fmt.Sprint(global.Config.Settings.Port)); e != nil {
 		log.Println(e)
 	}
 }
